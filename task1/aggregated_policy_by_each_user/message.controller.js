0ee1710231adeac785b24a7ae4762f249464fff1
shafi2021 <59860796+shafi2021@users.noreply.github.com>
2026-01-12 20:27:09 +0530

    Create message.controller.js

---
+++ task1/aggregated_policy_by_each_user/message.controller.js
@@ -0,0 +1,14 @@
+// GET /api/policy/aggregate
+
+router.get("/aggregate", async (req, res) => {
+  const data = await Policy.aggregate([
+    {
+      $group: {
+        _id: "$userId",
+        totalPolicies: { $sum: 1 }
+      }
+    }
+  ]);
+
+  res.json(data);
+});
