6fd55140acba4157d321cb6bdfc554a98ad88f96
shafi2021 <59860796+shafi2021@users.noreply.github.com>
2026-01-12 21:23:56 +0530

    Create policy.controller.js

---
+++ task1/src/controllers/policy.controller.js
@@ -0,0 +1,7 @@
+router.get("/search", async (req, res) => {
+  const user = await User.findOne({ firstName: req.query.username });
+  const policies = await Policy.find({ userId: user._id })
+    .populate("categoryId carrierId agentId");
+
+  res.json(policies);
+});
