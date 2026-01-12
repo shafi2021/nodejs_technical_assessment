51d919cbef2676e9b6e0da98eeac88269adbdf72
shafi2021 <59860796+shafi2021@users.noreply.github.com>
2026-01-12 21:16:13 +0530

    Create app.js

---
+++ task1/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("./config/db");
+const cpuMonitor = require("./utils/cpuMonitor");
+
+const uploadRoutes = require("./routes/upload.routes");
+const policyRoutes = require("./routes/policy.routes");
+const messageRoutes = require("./routes/message.routes");
+
+const app = express();
+
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api/upload", uploadRoutes);
+app.use("/api/policy", policyRoutes);
+app.use("/api/message", messageRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+cpuMonitor();
+
+module.exports = app;
