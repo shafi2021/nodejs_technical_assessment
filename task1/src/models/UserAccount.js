3fbc5622c9e5000baef7191ac7c98c3f23dbbe28
shafi2021 <59860796+shafi2021@users.noreply.github.com>
2026-01-12 20:48:37 +0530

    Create UserAccount.js

---
+++ task1/src/models/UserAccount.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UserAccountSchema = new mongoose.Schema(
+  {
+    accountName: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("UserAccount", UserAccountSchema);
